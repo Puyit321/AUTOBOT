9d30f6b3b6810fb9eed264999f7b779aa32163d5
Puyit321 <jkingpuyit@gmail.com>
2024-12-10 15:38:53 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -19,45 +19,32 @@ module.exports.run = async function({ api, event, args }) {
   const input = args.join(' ');
 
   if (!input) {
-    return api.sendMessage(`Please provide a question or statement after 'ai'. For example: 'ai What is the capital of France?'`, event.threadID, event.messageID);
+    api.sendMessage(`Please provide a question or statement after 'ai'. For example: 'ai What is the capital of France?'`, event.threadID, event.messageID);
+    return;
   }
-
+  
   if (input === "clear") {
     try {
-      await axios.post('https://gaypt4ai.onrender.com/clear', { id: event.senderID });
+      await axios.post('https://gpt-4-cfgh.onrender.com/clear', { id: event.senderID });
       return api.sendMessage("Chat history has been cleared.", event.threadID, event.messageID);
-    } catch (error) {
-      console.error(error);
+    } catch {
       return api.sendMessage('An error occurred while clearing the chat history.', event.threadID, event.messageID);
     }
   }
-
-
-  let chatInfoMessageID = "";
   
-  api.sendMessage(`🔍 "${input}"`, event.threadID, (error, chatInfo) => {
-    chatInfoMessageID = chatInfo.messageID;
-  },event.messageID);
-
   try {
-    const url = (event.type === "message_reply" && event.messageReply.attachments[0]?.type === "photo")
+    const url = event.type === "message_reply" && event.messageReply.attachments[0]?.type === "photo"
       ? { link: event.messageReply.attachments[0].url }
       : {};
 
-    const { data } = await axios.post('https://gays-porno-api.onrender.com/chat', {
+    const { data } = await axios.post('https://gpt-4-cfgh.onrender.com/chat', {
       prompt: input,
       customId: event.senderID,
       ...url
     });
 
-    api.editMessage(`${data.message}`, chatInfoMessageID, (err) => {
-      if (err) {
-        console.error(err);
-      }
-    });
-
-  } catch (error) {
-    console.error(error);
-    return api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
+    api.sendMessage(`•| 𝙱𝙾𝙶𝙰𝚁𝚃 𝙰𝙸 𝙱𝙾𝚃 |•\n\n${data.message}\n\n•| 𝙲𝚁𝙴𝙰𝚃𝙴𝙳 𝙱𝚈 𝙱𝙾𝙶𝙰𝚁𝚃 𝙼𝙰𝙶𝙰𝙻𝙿𝙾𝙺 |•`, event.threadID, event.messageID);
+  } catch {
+    api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
 };
